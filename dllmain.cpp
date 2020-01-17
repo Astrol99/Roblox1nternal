8f7d910e1c72b4da341b947870b6fc00fc7b1d6a
David <dngames2006@gmail.com>
2020-01-17 02:48:27 -0600

    Basic Cheat Structure

--- dllmain.cpp
+++ dllmain.cpp
@@ -5,6 +5,27 @@
 #include <TlHelp32.h>
 #include "mem.h"
 
+DWORD WINAPI HackThread(HMODULE hModule) {
+    
+    // Create Console
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+
+    std::cout << "[+] Opened Console" << std::endl;
+
+    // Close console
+    std::cout << "[*] Cleaning up..." << std::endl;
+
+    fclose(f);
+    FreeConsole();
+    
+    // Clean & Eject
+    FreeLibraryAndExitThread(hModule, 0);
+
+    return 0;
+}
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -13,6 +34,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+    {
+        HANDLE hThread = nullptr;
+        hThread = CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr);
+
+        if (hThread)
+            CloseHandle(hThread)
+    }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
