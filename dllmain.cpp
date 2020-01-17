d441a476c18cf1057a23ce918d6f6d364a6d1146
David <dngames2006@gmail.com>
2020-01-17 02:40:30 -0600

    Added required dependencies

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,5 +1,9 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <Windows.h>
+#include <iostream>
+#include <TlHelp32.h>
+#include "mem.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
